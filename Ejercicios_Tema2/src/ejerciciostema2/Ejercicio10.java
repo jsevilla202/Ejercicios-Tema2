0677b7922055b0ed0797ba601c9b54980920ae2a
jsevilla202 <jsevilla@iesnervion.es>
2022-10-10 09:10:59 +0200

    Ejercicios Tema 2 comentados

--- Ejercicios_Tema2/src/ejerciciostema2/Ejercicio10.java
+++ Ejercicios_Tema2/src/ejerciciostema2/Ejercicio10.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
+		//Nombramos las variables como numeros enteros
 		int num1, num2, num3;
-		
+		//Creamos el scanner y lo nombramos "sc"
 		Scanner sc = new Scanner(System.in);
-		
+		//Anuncioamos al usuario lo que se le va a pedir a continuación
 		System.out.println("A continuación inserte 3 numeros cuando se lo pidan y se comprobará si el"
 				+ " tercero es la suma del primero y segundo");
-		
+		/*Pedimos al usuario inserte 3 numeros, 1 cada vez que le sea requerido, capturamos las respuestas
+		y las guardamos en las variables "num1", "num2" y "num3" respectivamente*/
 		System.out.print("Inserte el primer número: ");
 		num1 = sc.nextInt();
 		System.out.print("Inserte el segundo número: ");
@@ -19,8 +21,12 @@ public class Ejercicio10 {
 		System.out.print("Inserte el tercer número: ");
 		num3 = sc.nextInt();
 		
+		//Si el sumatorio de num1 y num2 es igual a num3 lo indicamos y si no pues indicamos de que no son
 		System.out.println(num3!=num1+num2? "El primer numero y el segundo no suman el tercero": 
 			"El primer numero y el segundo suman el tercero");
+		
+		//Cerramos el scanner
+		sc.close();
 	}
 
 }
