e4c3b9aef18a271ccfb4128427248b5e209cc24b
jsevilla202 <jsevilla@iesnervion.es>
2022-10-10 09:26:04 +0200

    Ejercicios Tema 2 comentados

--- Ejercicios_Tema2/src/ejerciciostema2/Ejercicio9_2.java
+++ Ejercicios_Tema2/src/ejerciciostema2/Ejercicio9_2.java
@@ -26,7 +26,8 @@ public class Ejercicio9_2 {
 		}
 		/*Si el jugador 1 ha sacado Piedra, Papel o Tijeras y el jugador 2 Papel, Tijeras o Piedra,
 		el jugador 2 gana*/
-		if(respuesta1==1&&respuesta2==2||respuesta1==2&&respuesta2==3||respuesta1==3&&respuesta2==1) {
+		if(respuesta1==PIEDRA&&respuesta2==PAPEL||respuesta1==PAPEL&&respuesta2==TIJERAS||
+				respuesta1==TIJERAS&&respuesta2==PIEDRA) {
 				System.out.println("El jugador 2 ha ganado");
 			}
 		//En otro caso gana el jugador 1
