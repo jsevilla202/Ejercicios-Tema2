a94b6861000274683764c5bb31e2c0f76622a640
jsevilla202 <jsevilla@iesnervion.es>
2022-10-11 10:32:10 +0200

    Ejercicios Tema 2 comentado

--- Ejercicios_Tema2/src/ejerciciostema2/Ejercicio8.java
+++ Ejercicios_Tema2/src/ejerciciostema2/Ejercicio8.java
@@ -16,17 +16,20 @@ public class Ejercicio8 {
 		
 		//Creamos un switch y dependiendo de la nota introducida pasará una acción u otra
 		switch(nota) {
-		case 0: System.out.print("la nota es INSUFUICIENTE");
-		case 1: System.out.print("la nota es INSUFUICIENTE");
-		case 2: System.out.print("la nota es INSUFUICIENTE");
-		case 3: System.out.print("la nota es INSUFUICIENTE");
-		case 4: System.out.print("la nota es INSUFUICIENTE");
-		case 5: System.out.print("la nota es SUFUICIENTE");
-		case 6: System.out.print("la nota es BIEN");
-		case 7: System.out.print("la nota es NOTABLE");
-		case 8: System.out.print("la nota es NOTABLE");
-		case 9: System.out.print("la nota es SOBRESALIENTE");
-		case 10: System.out.print("la nota es SOBRESALIENTE");
+		case 0: System.out.print("la nota es INSUFUICIENTE"); break;
+		case 1: System.out.print("la nota es INSUFUICIENTE"); break;
+		case 2: System.out.print("la nota es INSUFUICIENTE"); break;
+		case 3: System.out.print("la nota es INSUFUICIENTE"); break;
+		case 4: System.out.print("la nota es INSUFUICIENTE"); break;
+		case 5: System.out.print("la nota es SUFUICIENTE");break;
+		case 6: System.out.print("la nota es BIEN"); break;
+		case 7: System.out.print("la nota es NOTABLE"); break;
+		case 8: System.out.print("la nota es NOTABLE"); break;
+		case 9: System.out.print("la nota es SOBRESALIENTE"); break;
+		case 10: System.out.print("la nota es SOBRESALIENTE"); break;
+		default:
+			System.out.print("La nota introducida no es correcta");
+			break;
 		}
 		//Cerramos el scanner
 		sc.close();
